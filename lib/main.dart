4d0e22b2893db115c0d1d7bce28d21fbad9a5895
Devbrat Anuragi <39759685+AslanDevbrat@users.noreply.github.com>
2021-07-22 14:28:09 +0530

    completed the magic 8 ball app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(BallPage());
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Ball(),
+    ));
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 0;
+  void genstate() {
+    setState(() {
+      ballNumber = Random().nextInt(4) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+            onPressed: () {
+              genstate();
+              print('I got Clicked');
+            },
+            child: Image.asset('images/ball$ballNumber.png'),
+          ))
+        ],
       ),
     );
+  }
+}
